b22d23abc2ea020c83040cd233f491339d3d2389
Radu ISCRA <iscra.r000@gmail.com>
2023-12-11 16:31:27 +0100

    Grand update du front end

--- projet_appli_frais/scripts.js
+++ projet_appli_frais/scripts.js
@@ -1,2 +1,15 @@
+function openModal() {
+    document.getElementById('myModal').style.display = 'flex';
+  }
 
-  
+  function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+  }
+
+  // Fermer la fenêtre modale si l'utilisateur clique en dehors du contenu
+  window.onclick = function(event) {
+    var modal = document.getElementById('myModal');
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  }
