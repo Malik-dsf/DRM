6edc436c41f0bf34ef683e3c85287e089c67a606
Radu ISCRA <iscra.r000@gmail.com>
2023-12-13 09:46:35 +0100

    Update page connexion

--- projet_appli_frais/index.php
+++ projet_appli_frais/index.php
@@ -7,7 +7,7 @@
 
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Connexion - DRM</title>
   <link rel="stylesheet" href="style.css">
   <link rel="icon" href="media/LogoSwissPharma.png" type="image/x-icon">
